9ce017ee421918c7b437cad9b5911ab207fa5a14
Liliherax <57782295+Gabriel-demian@users.noreply.github.com>
2023-11-14 23:19:28 -0300

    config para obtener archivos de configuración de un repo

--- loans/src/main/java/com/bankApp/loans/LoansApplication.java
+++ loans/src/main/java/com/bankApp/loans/LoansApplication.java
@@ -30,9 +30,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		)
 )
 public class LoansApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoansApplication.class, args);
 	}
-
 }
