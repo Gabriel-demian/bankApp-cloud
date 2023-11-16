164b9704ed39b88c18d8e2ab3963f6af43a3195b
Liliherax <57782295+Gabriel-demian@users.noreply.github.com>
2023-11-16 00:42:07 -0300

    se agrega rabbitmq para actualizar todas las properties de los microservicios al mismo tiempo. Se agrega Webhook para que se actualicen los microservicios automaticamente cada vez que se modifican las properties en github

--- accounts/src/main/java/com/bankApp/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/bankApp/accounts/dto/AccountsContactInfoDto.java
@@ -1,5 +1,7 @@
 package com.bankApp.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -9,13 +11,15 @@ import java.util.Map;
  * Represents contact information for accounts. This class is used to map properties
  * prefixed with "accounts" from the application configuration.
  *
- * @param message           A message related to the account contact information.
- * @param contactDetails    A map containing contact details, where keys represent
- *                          contact types, and values represent corresponding details.
- * @param onCallSupport     A list of strings representing on-call support details.
- *
  * @see org.springframework.boot.context.properties.ConfigurationProperties
  */
 @ConfigurationProperties(prefix= "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport ) {
+@Getter
+@Setter
+public class AccountsContactInfoDto{
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
